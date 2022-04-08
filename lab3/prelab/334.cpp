902e733367045f877b6b365ee362313dbbf11634
Nguyễn Ngô Huy <ngohuy200338@gmail.com>
2022-04-08 23:21:51 +0700

    Create 334.cpp

---
+++ lab3/prelab/334.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+};
+node *createLinkedList(int n)
+{
+    // TO DO
+    node* head = NULL;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        node* newNode = new node{x, NULL};
+        newNode -> next = head;
+        head = newNode;
+    }
+    return head;
+}
+void print(node *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data << endl;
+        head = head->next;
+    }
+}
+int main()
+{
+    int n = 0;
+    cin >> n;
+    if (n > 0)
+    {
+        node *head = createLinkedList(n);
+        print(head);
+    }
+    else
+    {
+        cout << "Invalid n" << endl;
+    }
+    return 0;
+}
